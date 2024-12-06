5a63a7f408b3e8158d10787ff52c31ba139d385d
ZiV99 <71887602+VitaminB7@users.noreply.github.com>
2024-12-06 15:55:45 +0700

    lab8_3.cpp Completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,33 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream>
+using namespace std;
+int main(){
+    double aryu,h,b;
+    cout<<"Enter your age: ";
+    cin>>aryu;
+    if(aryu<=25){
+        cout<<"Enter your height: ";
+        cin>>h;
+        if(h<100)cout<<"Your character = Chopper";
+        else if(h<180)cout<<"Your character = Usopp";
+        else{
+            cout<<"Enter your bounty: ";
+            cin>>b; 
+            if(b>1100000000){cout<<"Your character = Zoro";}
+            else cout<<"Your character = Sanji";}
+        
+    }  
+    
+    else if(aryu<=60) {
+        cout<<"Enter your bounty: ";
+        cin>>b; 
+        if(b>500000000)cout<<"Your character = Jinbe";
+        else cout<<"Your character = Franky";
+        
+    }  
+    
+    else cout<<"Your character = Brook";
+ 
+    
+    
+    
+}
